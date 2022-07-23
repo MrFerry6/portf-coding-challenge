afc1a8684e714e44c5a5712b3856b039c67aae68
ferry <ferrysanchez77@gmail.com>
2022-07-23 23:47:48 +0100

    Add recursive loop get all beers

--- src/BeerResponsiveBar.js
+++ src/BeerResponsiveBar.js
@@ -6,45 +6,49 @@ import { useEffect, useState } from "react";
 
 
 const BeerResponsiveBar = () => {
-
+  
   const [beers, setBeers] = useState([{}])
 
   useEffect(() => {
-    var requestOptions = {
+    var entries = []
+    getAllbeers(1);
+
+    function getAllbeers(page) {
+      var requestOptions = getRequestOptions()
+      let url = "https://api.punkapi.com/v2/beers?page=" + page + "&per_page=80"
+
+      fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          const entriesResultObject = JSON.parse(result);
+          if (entriesResultObject.length > 0) {
+            for (let entrie of entriesResultObject) {
+              entries.push(entrie)
+            }
+            getAllbeers(page + 1)
+
+            console.log(entries)
+          }
+          if (entriesResultObject.length === 0) {
+            setBeers(entries)
+          }
+        })
+        .catch(error => console.log('error', error));
+    }
+  }, [])
+  function getRequestOptions() {
+    return {
       method: 'GET',
       headers: {
         "Content-Type": "application/json"
       },
       redirect: 'follow'
-    }
-
-    fetch("https://api.punkapi.com/v2/beers", requestOptions)
-      .then(response => response.text())
-      .then(result => {
-        const entriesResult = JSON.parse(result)
-        let entries = [];
-        for (let entrie of entriesResult){
-          entries.push(entrie)
-        }
-        setBeers(entries)
-      })
-      .catch(error => console.log('error', error));
-  }, [])
-
-
+    };
+  }
 
   return (
     <div>{beers || null ? beers.map(beers => <div>{beers.name}</div>) : <div>"No hay chervecha"</div>}</div>
   );
-  /*var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.punkapi.com/v2/beers", requestOptions)
-    .then(response => response.text())
-    //.then(result => {console.log(result)})
-    .catch(error => console.log('error', error));
-   */
+
 }
 export default BeerResponsiveBar;
