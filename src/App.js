554d86bb4b380e9fa605de7569ac35b2edf01c69
ferry <ferrysanchez77@gmail.com>
2022-07-23 05:11:42 +0100

    Data parameter adeed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import BeerResponsiveBar from './BeerResponsiveBar';
 import './App.css';
 
+const data = [
+  { example: 1, value: 250 },
+  { example: 2, value: 25 },
+  { example: 3, value: 3500 }
+];
 
 
 function App() {
   return (
     <div className="App">
-    <BeerResponsiveBar/>
+    <BeerResponsiveBar data={data}/>
     </div>
   );
 }
--- src/BeerResponsiveBar.js
+++ src/BeerResponsiveBar.js
@@ -1,13 +1,7 @@
 import ResponsiveBar from "nivo/lib/components/charts/bar/ResponsiveBar";
 
-const data = [
-  { example: 1, value: 250 },
-  { example: 2, value: 25 },
-  { example: 3, value: 3500 }
-];
 
-
-function BeerResponsiveBar() {
+const BeerResponsiveBar = ({data}) =>{
   return (
     <div style={{ height: "250px" }}>
       <ResponsiveBar data={data} keys={["example"]} indexBy="value" />
