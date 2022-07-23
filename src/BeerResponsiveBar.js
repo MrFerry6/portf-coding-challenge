c9cd3823d4a2c72691d4e6dfb3b89461d6bc00a5
ferry <ferrysanchez77@gmail.com>
2022-07-23 22:05:04 +0100

    Fetch data import beers

--- src/BeerResponsiveBar.js
+++ src/BeerResponsiveBar.js
@@ -1,26 +1,50 @@
 import ResponsiveBar from "nivo/lib/components/charts/bar/ResponsiveBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 
 
-const BeerResponsiveBar = () =>{
+const BeerResponsiveBar = () => {
 
-  const [beers, setBeers] = useState()
-/*var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+  const [beers, setBeers] = useState([{}])
 
-fetch("https://api.punkapi.com/v2/beers", requestOptions)
-  .then(response => response.text())
-  //.then(result => {console.log(result)})
-  .catch(error => console.log('error', error));
- */
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      redirect: 'follow'
+    }
+
+    fetch("https://api.punkapi.com/v2/beers", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        const entriesResult = JSON.parse(result)
+        let entries = [];
+        for (let entrie of entriesResult){
+          entries.push(entrie)
+        }
+        setBeers(entries)
+      })
+      .catch(error => console.log('error', error));
+  }, [])
+
+
+
+  return (
+    <div>{beers || null ? beers.map(beers => <div>{beers.name}</div>) : <div>"No hay chervecha"</div>}</div>
+  );
+  /*var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.punkapi.com/v2/beers", requestOptions)
+    .then(response => response.text())
+    //.then(result => {console.log(result)})
+    .catch(error => console.log('error', error));
+   */
 }
-
-return (
-  <div>{ beers || null ? beers.map(beers => <div>{beers.name}</div>) :  <div>"No hay chervecha"</div>  }</div>
-);
-export default  BeerResponsiveBar;
+export default BeerResponsiveBar;
