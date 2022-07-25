e08216a3d7efcd92c828824c3caa30c31915681a
ferry <ferrysanchez77@gmail.com>
2022-07-25 05:53:52 +0100

    Range bettwen two selections at DatePikers

--- src/Filters.js
+++ src/Filters.js
@@ -8,11 +8,24 @@ import "react-datepicker/dist/react-datepicker.css"
 
 const Filter = () => {
     const [startDate, setStartDate] = useState(new Date());    
-    const [finishDate, setFinishDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
     return (
         <>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-            <DatePicker selected={finishDate} onChange={(date) => setFinishDate(date)} />
+            <DatePicker 
+            selected={startDate} 
+            onChange={(date) => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate} 
+            />
+            <DatePicker 
+            selected={endDate} 
+            onChange={(date) => setEndDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}  
+            />
         </>
     );
 };
