8be89c74a3e9d781cf5cdb4f808c6747c319a5f8
ferry <ferrysanchez77@gmail.com>
2022-07-27 04:50:37 +0100

    try responsive bar properties

--- src/BeerResponsiveBar.js
+++ src/BeerResponsiveBar.js
@@ -114,22 +114,9 @@ const BeerResponsiveBar = () => {
         data={isFromPiked ? groupsByFilterDate : groupsByDate}
         keys={["totalBeers"]}
         indexBy="date"
-        minValue={0}
-        maxValue={50}
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.4}
-        valueScale={{ type: "linear" }}
-        colors="#3182CE"
-        animate={true}
-        enableLabel={true}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "One legend",
-          legendPosition: "start",
-          legendOffset: -40
-        }}
+        margin={{ top: 20, right: 10, bottom: 50, left: 50}}
+        padding={0.1}
+       
       />
     </div>
 
